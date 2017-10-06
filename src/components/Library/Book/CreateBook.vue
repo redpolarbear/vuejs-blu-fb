4e41f86a064233cbbac99f4689a3a5ac2d47e86a
Matthew Xu <mxu@counterpath.com>
2017-10-05 22:52:02 -0700

    newBook

--- src/components/Library/Book/CreateBook.vue
+++ src/components/Library/Book/CreateBook.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="container">
-      This is the Create New Book
-  </div>
+  <section class="section">
+    <div class="container">
+      <p class="control has-addons has-addons-centered has-icon has-icon-right">
+        <input class="input is-large" type="text" placeholder="ISBN Code">
+        <a class="button is-large">
+          <span class="icon is-medium">
+            <i class="fa fa-camera"></i>
+          </span> 
+        </a>
+      </p>
+      <p class="control has-text-centered">
+        <a class="button is-primary is-large">
+          Search
+        </a>
+      </p>
+    </div>
+  </section>
 </template>
 
 <script>
